b9dedc1d320877d48d06fb08296d9aa8bc9ac47d
Lera Savchenko <savchenko.valeriya@gmail.com>
2017-04-26 00:28:54 +0300

    listing-page responsive slider skeleton

--- src/js/map.js
+++ src/js/map.js
@@ -4,16 +4,24 @@ function init_map()
 				zoom:16,
 				center:new google.maps.LatLng(50.4501,30.523400000000038),mapTypeId: google.maps.MapTypeId.ROADMAP
 			};
+			
 			map = new google.maps.Map(document.getElementById('map-canvas'), myOptions);
 			marker = new google.maps.Marker(
 				{
 					map: map,
 					position: new google.maps.LatLng(50.4501,30.523400000000038)
 				});
-			infowindow = new google.maps.InfoWindow({content:'<strong>Location</strong><br>Kiev, Ukraine<br>'});
-			google.maps.event.addListener(marker, 'click', function(){
+			
+			infowindow = new google.maps.InfoWindow(
+				{
+					content:'<strong>Location</strong><br>Kiev, Ukraine<br>'
+				});
+			
+			google.maps.event.addListener(marker, 'click', function()
+			{
 				infowindow.open(map,marker);
 			});
 			infowindow.open(map,marker);
 		}
-		google.maps.event.addDomListener(window, 'load', init_map);
+
+google.maps.event.addDomListener(window, 'load', init_map);
